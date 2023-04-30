7e0b9fabfa0610891d529ebf0691475c8dbc9473
fabidota <62340981+fabidota@users.noreply.github.com>
2023-04-30 18:10:44 -0300

    Inclusão do template Yummy

--- Application/aspnet-core-dotnet-core/Controllers/HomeController.cs
+++ Application/aspnet-core-dotnet-core/Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace AlimentacaoInfantil.Controllers
             return View(); 
         }
 
+        public IActionResult HomeYummy()
+        {
+            return View();
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
