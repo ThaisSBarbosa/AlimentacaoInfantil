2fcacf2662b6036e8abb2646b0b630d5d73915ca
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2023-05-01 21:56:43 -0300

    Testando variável de ambiente.

--- Application/aspnet-core-dotnet-core/DAO/ConexaoBD.cs
+++ Application/aspnet-core-dotnet-core/DAO/ConexaoBD.cs
@@ -1,5 +1,7 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using aspnet_core_dotnet_core.Utils;
+using Microsoft.Extensions.Configuration;
 using MySql.Data.MySqlClient;
+using System;
 
 namespace AlimentacaoInfantil.DAO
 {
@@ -11,10 +13,17 @@ namespace AlimentacaoInfantil.DAO
         /// <returns>Conexão aberta</returns>
         public static MySqlConnection GetConexao(IConfiguration config)
         {
-            string strConn = config.GetSection("ConnectionString").Value;
-            MySqlConnection conexao = new MySqlConnection(strConn);
+            MySqlConnection conexao = new MySqlConnection(GetConnectionString(config));
             conexao.Open();
             return conexao;
+        }
+
+        private static string GetConnectionString(IConfiguration config)
+        {
+            string strConn = config.GetSection(Constantes.STR_CONN_LOCAL).Value;
+            if (string.IsNullOrEmpty(strConn))
+                strConn = Environment.GetEnvironmentVariable(Constantes.STR_CONN_AZURE);
+            return strConn;
         }
     }
 }
---
+++ Application/aspnet-core-dotnet-core/Utils/Constantes.cs
@@ -0,0 +1,8 @@
+﻿namespace aspnet_core_dotnet_core.Utils
+{
+    public static class Constantes
+    {
+        public const string STR_CONN_AZURE = "MYSQLCONNSTR_ConnectionString";
+        public const string STR_CONN_LOCAL = "ConnectionString";
+    }
+}
