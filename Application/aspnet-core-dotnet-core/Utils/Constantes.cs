fbc3e35325e63f580acdd1dce6e312f2d676249d
ThaisSBarbosa <thssantosbarbosa@gmail.com>
2023-05-02 00:31:19 -0300

    Testando segredo do jwt como variável de ambiente.

--- Application/aspnet-core-dotnet-core/Utils/Constantes.cs
+++ Application/aspnet-core-dotnet-core/Utils/Constantes.cs
@@ -4,6 +4,6 @@
     {
         public const string STR_CONN_AZURE = "MYSQLCONNSTR_ConnectionString";
         public const string STR_CONN_LOCAL = "ConnectionString";
-        public const string SECRET_KEY = "SecretKey";
+        public const string SECRET_KEY = "APPSETTING_SecretKey";
     }
 }
