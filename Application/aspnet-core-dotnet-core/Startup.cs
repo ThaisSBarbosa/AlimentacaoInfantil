12ac7f2a785abeb60ba5f9b5a44d412e43ef2f1b
jessicapradocosta <jessicapradocosta@gmail.com>
2023-05-07 19:58:10 -0300

    ajustes de texto em tópico na seção "sobre nós" da página home e ajustes de conteúdo na página "posts"

--- Application/aspnet-core-dotnet-core/Startup.cs
+++ Application/aspnet-core-dotnet-core/Startup.cs
@@ -57,7 +57,7 @@ namespace aspnet_core_dotnet_core
                 {
                     endpoints.MapControllerRoute(
                         name: "default",
-                        pattern: "{controller=Home}/{action=Index}/{id?}");
+                        pattern: "{controller=Home}/{action=HomeYummy}/{id?}");
                 });
             });
         }
